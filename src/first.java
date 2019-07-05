fe2edb59c73ce8dd1c5a4067033e2fcc58efb750
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-06 00:39:31 +0300

    firstQ

--- src/first.java
+++ src/first.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 
-public class Main {
+/**
+ * Находит в строке лишние пробелы (2 и более, в начале, в конце) и убрать их
+ * https://vk.com/topic-123851409_39456559
+ */
 
+public class first {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
-//        String message=in.nextLine().replace("  ","");
         String message=in.nextLine().replaceAll("^ | $| {2}","");
-
-        int leng = message.length();
-/**/
-/*        if (message.substring(0,1).equals(" "))
-            message.substring(0,1).re*/
         System.out.println(message);
     }
 }
