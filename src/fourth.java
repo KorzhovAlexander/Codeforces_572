1c630539b604163b8c588902090d36e5333425a6
KorzhovAlexander <honeyiamgood@mail.ru>
2019-07-07 23:32:47 +0300

    fourthQ

--- src/fourth.java
+++ src/fourth.java
@@ -10,15 +10,29 @@ import java.util.Scanner;
 **.**
 *...*
 *****
+2 2
+
+5
+*****
+**.**
+*..**
+*...*
+*****
+3 2
 
 * */
 public class fourth {
+    private static int pos;
+    private static int[][] list;
+    private static boolean[][] used;
+    private static int rez;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int pos = sc.nextInt();
+        pos = sc.nextInt();
 
-        char[][] list = new char[pos][pos];
+        list = new int[pos][pos];
         for (int i = 0; i < pos; i++) {
             String scanner = sc.next();
             for (int j = 0; j < pos; j++) {
@@ -26,8 +40,36 @@ public class fourth {
             }
         }
 
-        boolean[] used = new boolean[list.length];
+        used = new boolean[pos][pos];
+
+        find(sc.nextInt()-1,sc.nextInt()-1);
+        System.out.println(" ");
+        System.out.println(Arrays.deepToString(list));
+        System.out.println(Arrays.deepToString(used));
+        System.out.println(rez);
     }
+
+    private static void find(int OX, int OY) {
+        if ((list[OX][OY]==42) || (used[OX][OY])){
+            return;
+        }
+        rez++;
+        used[OX][OY] = true;
+        if (list[OX][OY+1]!=42){
+            find(OX,OY+1);
+        }
+        if (list[OX+1][OY]!=42){
+            find(OX+1,OY);
+        }
+        if (list[OX-1][OY]!=42){
+            find(OX-1,OY);
+        }
+        if (list[OX][OY-1]!=42){
+            find(OX,OY-1);
+        }
+
+    }
+
 
 
 }
